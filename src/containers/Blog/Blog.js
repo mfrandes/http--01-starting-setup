0f853dc9ac53aa6bc0870c65b01ca71e5ec23693
Mircea <frandesmyrce@gmail.com>
2020-05-09 00:37:09 +0300

    Catch and hadle error with axios

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import './Blog.css';
 class Blog extends Component {
     state = {
         posts: [],
-        selectedPostId: null
+        selectedPostId: null,
+        error: false
     }
 
     componentDidMount() {
@@ -23,25 +24,33 @@ class Blog extends Component {
                     }
                 })
                 this.setState({ posts: updatedData })
-                 console.log(response); 
+                console.log(response);
+            })
+            .catch(error => {
+                // console.log(error); 
+                this.setState({ error: true })
             });
     }
 
     postSelectedHandler = id => {
         console.log('Bazinga', id);
-        
+
         this.setState({ selectedPostId: id })
     }
 
     render() {
-        const posts = this.state.posts.map(posts => {
-            return <Post
-                title={posts.title}
-                key={posts.id}
-                author={posts.author}
-                clicked={() => this.postSelectedHandler(posts.id)}
-            />
-        })
+        let posts = <p style={{ textAlign: 'center' }}>Somesing went wrong</p>
+        if (!this.state.error) {
+            posts = this.state.posts.map(posts => {
+                return <Post
+                    title={posts.title}
+                    key={posts.id}
+                    author={posts.author}
+                    clicked={() => this.postSelectedHandler(posts.id)}
+                />
+            })
+        }
+
         return (
             <div>
                 <section className="Posts">
