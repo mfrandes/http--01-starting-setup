1d94f6625ee75fed2d58330c1126a045902d0730
Mircea <frandesmyrce@gmail.com>
2020-05-08 23:15:32 +0300

    creating get request in componentDidMount

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,13 +7,37 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    render () {
+    state = {
+        posts: []
+    }
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                const posts = response.data.slice(0, 4);
+                const updatedData = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Edd'
+                    }
+                })
+                this.setState({ posts: updatedData })
+                //  console.log(response); 
+            });
+    }
+
+    render() {
+        const posts = this.state.posts.map(posts => {
+            return <Post 
+            title={posts.title}
+            key={posts.is}
+            author={posts.author}
+            />
+        })
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                   {posts}
                 </section>
                 <section>
                     <FullPost />
